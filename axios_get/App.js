630103d4a4b252a2448d05d3393253882ab39ccb
BrunoDuranTeodoro <duranteodoro20@gmail.com>
2025-09-05 20:29:56 -0300

    inserçãodoobjetoapiemockup

--- axios_get/App.js
+++ axios_get/App.js
@@ -5,4 +5,37 @@ import api from "./src/devices/api";
 export default App(){
 
   const[users, setUsers] = useState([]);
+
+  const API = "http://localhost:3000/users"
+
+  async function fetchUsers() {
+    try{
+      const response = await api.get(API);
+      setUsers(response.data)
+    }catch(error){
+      console.error("Error GET:", error.message);
+    }
+  }
+
+  useEffect(()=>{
+    fetchUsers();
+  },[])
+
+  const _render() => {
+    const vet = [];
+    users.map((item, index) => {
+      vet.push(
+        <View key={index}>
+          <Text style={styles.item}>
+              ID:{item.id}
+              NOME:{item.name}
+              EMAIL:{item.email}
+          </Text>
+        </View>
+      )
+    })
+    return vet;
+  }
+
+  
 }

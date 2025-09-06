94f7a35dd6affe86625f15eb593e99ce3bd9bdd9
BrunoDuranTeodoro <duranteodoro20@gmail.com>
2025-09-05 21:09:49 -0300

    o

--- axios_get/App.js
+++ axios_get/App.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState} from "react";
 import { View, Text, ScrollView, StyleSheet, Button } from "react-native";
 import api from "./src/devices/api";
 
-export default App(){
+export default function App(){
 
   const[users, setUsers] = useState([]);
 
-  const API = "http://localhost:3000/users"
+  const API = "http:10.110.12.40:3000/users"
 
   async function fetchUsers() {
     try{
@@ -21,7 +21,7 @@ export default App(){
     fetchUsers();
   },[])
 
-  const _render() => {
+  const _render = () => {
     const vet = [];
     users.map((item, index) => {
       vet.push(
@@ -37,5 +37,21 @@ export default App(){
     return vet;
   }
 
-  
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Get - Listar Usuarios
+      </Text>
+      <Button title="recarregar lista" onPress={fetchUsers}> </Button>
+      <ScrollView>
+        {_render()}
+      </ScrollView>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container : {flex:1, padding:20, marginTop:40},
+  title : {fontSize:22, fontWeight: "bold", marginBottom: 10},
+  item : {fontSize:12, marginTop:10}
+})
